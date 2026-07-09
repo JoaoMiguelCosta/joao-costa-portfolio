b0e23cce82c525c390173e11611ab42a734dbd5f
JoaoMiguelCosta <joaoxxmiguel@hotmail.com>
2026-07-09 23:11:34 +0100

    refactor(i18n): use section translation modules

--- src/i18n/translations/en/index.js
+++ src/i18n/translations/en/index.js
@@ -1,6 +1,6 @@
 import header from "./header.js";
-import home from "./home.js";
-import projectsPage from "./projectsPage.js";
+import home from "./home/index.js";
+import projectsPage from "./projectsPage/index.js";
 import footer from "./footer.js";
 import feedback from "./feedback.js";
 
